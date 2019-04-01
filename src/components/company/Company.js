c2b9ceeb8d1d35078f07ade16b8bce9a9177f564
konrad.kujawa <konrad.kujawa@pragmatists.pl>
2019-04-01 13:31:07 +0200

    conditional company quote rendering

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './Company.css';
 import QuoteChange from '../quote/QuoteChange';
+import { isEmpty } from 'lodash';
 
 export default class Company extends React.Component {
+
+  renderCompanyQuote(company) {
+    if (isEmpty(company.quote)) {
+      return <div className="company-details-row company-details-price"> </div>
+    }
+    return (
+      <div className="company-details-row company-details-price">
+        <span>{company.quote.price}</span>
+        <span> {company.currency}</span>
+        <QuoteChange change={company.quote.change} changePercent={company.quote.percent} />
+        <span>Closed: {company.quote.day}</span>
+      </div>
+    );
+  }
 
   render() {
     const company = this.props.company;
     return (
       <div className="company">
         <div className="company-logo">
-          <img src={company.logo} alt={company.name}/>
+          <img src={company.logo} alt={company.name} />
         </div>
         <div className="company-details">
           <div className="company-details-row company-details-name">
@@ -23,12 +38,7 @@ export default class Company extends React.Component {
             <span>{company.marketClose} </span>
             <span>{company.timezone}</span>
           </div>
-          <div className="company-details-row company-details-price">
-            <span>{company.price}</span>
-            <span> {company.currency}</span>
-            <QuoteChange change={company.change} changePercent={company.changePercent} />
-            <span>Closed: {company.latestTradingDay}</span>
-          </div>
+          {this.renderCompanyQuote(company)}
         </div>
         <div>
           <button onClick={() => this.props.removeCompany(company)}>x</button>
--- src/components/quote/QuoteChange.js
+++ src/components/quote/QuoteChange.js
@@ -3,16 +3,17 @@ import './QuoteChange.css';
 
 export default class QuoteChange extends React.Component {
 
-  renderArrow() {
+  renderQuoteChangeArrow() {
     if (this.props.change < 0) {
       return <span className="quote-change-low">{this.props.change} ({this.props.changePercent}) &#8681;</span>;
     }
     return <span className="quote-change-high">+{this.props.change} (+{this.props.changePercent}) &#8679;</span>
   }
+
   render() {
     return (
       <div className="quote-change">
-        {this.renderArrow()}
+        {this.renderQuoteChangeArrow()}
       </div>
     )
   }
